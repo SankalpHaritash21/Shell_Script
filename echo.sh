6cec522d21e25c43d1808b5f113e3465b161aec2
Sankalp <sanklpkaushik21@gmail.com>
2024-07-29 08:27:09 +0530

    User and System define Variables

--- echo.sh
+++ echo.sh
@@ -7,3 +7,8 @@ echo -e "\033[0;32m success message here"
 echo -e "\033[0;33m Warning message here"
 
 
+echo "I am \t Sankalp Haritash"
+echo "I am \v Sankalp Haritash"
+
+echo -e "I am \t Sankalp Haritash"
+echo -e "I am \v Sankalp Haritash"
---
+++ variable.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# User-defined variables
+name="Sankalp"
+age="21"
+
+echo "My name is $name and my age is $age"
+
+# System-defined variables
+echo "My Name is Sankalp Haritash this is System define variables"
+
+
+echo "Use #env# command to find system variables"
+echo $HOME
+echo $OSTYPE
